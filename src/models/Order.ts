1a58fa92def99ce8d132bd17adf09f6e85b536da
Mo-wahid <mowahid923@gmail.com>
2026-07-08 15:39:11 +0500

    created cart, order and product models

---
+++ src/models/Order.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const OrderItemSchema = new Schema({
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+})
+
+const OrderSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    orderItems: [OrderItemSchema],
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    orderStatus: {
+        type: String,
+        enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
+        default: "Pending"
+    },
+},{timestamps: true}
+)
+
+const Order = models.Order || model("Order", OrderSchema);
+export default Order;
